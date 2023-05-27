4df9ca62ca1d689c2cb1c706f0dcfb9ddbbc246c
Andrew Tait Gehrhardt <atgehrhardt@Andrews-MacBook-Pro.local>
2023-05-27 01:41:19 -0500

    Cleaned up tons of code. Added popup modal and button that allows instructions and dynamic content for each board. Reworked boards directory structure and a bunch of code in app.js and index.ejs to support this functionality properly. Ideally, I will also add logic to scan for logic.js in each board subdirectory to allow scoring to be done through the modal.

--- app.js
+++ app.js
@@ -1,6 +1,10 @@
+/* /Users/atgehrhardt/Dev/TableTap/app.js */
 const express = require('express');
 const fs = require('fs');
 const path = require('path');
+const util = require('util');
+
+const readFile = util.promisify(fs.readFile);
 
 const app = express();
 
@@ -14,17 +18,38 @@ app.set('views', path.join(__dirname, 'views'));
 // Read the images from the 'boards' directory
 const boardsDirectory = path.join(__dirname, 'public', 'boards');
 
-const getBoardImages = () => {
-  return fs.readdirSync(boardsDirectory).filter(file => {
-    const extension = path.extname(file).toLowerCase();
-    return extension === '.jpg' || extension === '.png' || extension === '.svg';
-  });
+const getBoardImages = async (dir = boardsDirectory, prefix = '') => {
+  let boardImages = [];
+  const files = fs.readdirSync(dir);
+
+  for (let file of files) {
+    const filePath = path.join(dir, file);
+    const stats = fs.statSync(filePath);
+
+    if (stats.isDirectory()) {
+      boardImages = [...boardImages, ...await getBoardImages(filePath, `${prefix}${file}/`)];
+    } else {
+      const extension = path.extname(file).toLowerCase();
+      if (extension === '.jpg' || extension === '.png' || extension === '.svg') {
+        const modalFilePath = path.join(dir, 'modal.json');
+        let modalContent = {};
+        try {
+          modalContent = JSON.parse(await readFile(modalFilePath, 'utf-8'));
+        } catch (err) {
+          console.error(err);
+        }
+        boardImages.push({ image: `${prefix}${file.slice(file.lastIndexOf('/') + 1)}`, modalContent });
+      }
+    }
+  }
+
+  return boardImages;
 };
 
 // Render the index page with the board images
-app.get('/', (req, res) => {
-  const boardImages = getBoardImages();
-  res.render('index', { boardImages });
+app.get('/', async (req, res) => {
+  const boardImages = await getBoardImages();
+  res.render('index', { activeView: 'boards', boardImages });
 });
 
 // Start the server
